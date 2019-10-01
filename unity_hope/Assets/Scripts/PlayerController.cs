31bef25d64f4e6b939b6b7a5841070b88afbe2f0
LimitToInfinity <21stLimitless@gmail.com>
2019-09-30 20:11:27 -0600

    Added player package, and fast bouyancy asset.

---
+++ unity_hope/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.CompareTag("Pick up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+        }
+    }
+
+}
