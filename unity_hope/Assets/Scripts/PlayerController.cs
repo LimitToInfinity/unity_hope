3ac85fb3172831749a6b1ced1ad1af4251fa272e
LimitToInfinity <21stLimitless@gmail.com>
2019-10-06 19:01:08 -0600

    Jumping added.

--- unity_hope/Assets/Scripts/PlayerController.cs
+++ unity_hope/Assets/Scripts/PlayerController.cs
@@ -5,31 +5,40 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float jumpSpeed;
 
     private Rigidbody rb;
-    private int count;
+    // private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        count = 0;
+        // count = 0;
     }
 
     void Update()
     {
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
         rb.AddForce(movement * speed);
     }
 
-    void OnTriggerEnter(Collider other)
+    void FixedUpdate()
     {
-       if(other.gameObject.CompareTag("Pick up"))
+        if (Input.GetKeyDown (KeyCode.Space))
         {
-            other.gameObject.SetActive(false);
-            count += 1;
+            rb.AddForce(new Vector3(0, jumpSpeed, 0), ForceMode.Impulse);
         }
     }
+
+    // void OnTriggerEnter(Collider other)
+    // {
+    //    if(other.gameObject.CompareTag("Pick up"))
+    //     {
+    //         other.gameObject.SetActive(false);
+    //         count += 1;
+    //     }
+    // }
 
 }
